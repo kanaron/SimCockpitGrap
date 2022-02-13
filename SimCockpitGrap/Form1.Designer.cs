ebefdac04a2f37112a2288dc9f7525c4ca531042
kanaron125@gmail.com <kanaron125@gmail.com>
2022-02-13 17:49:35 +0100

    Add project files.

---
+++ SimCockpitGrap/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace SimCockpitGrap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SendButton = new System.Windows.Forms.Button();
+            this.RightFuel = new System.Windows.Forms.ComboBox();
+            this.Amps = new System.Windows.Forms.ComboBox();
+            this.LeftFuel = new System.Windows.Forms.ComboBox();
+            this.Tit = new System.Windows.Forms.ComboBox();
+            this.CHT = new System.Windows.Forms.ComboBox();
+            this.OilTemp = new System.Windows.Forms.ComboBox();
+            this.FuelPress = new System.Windows.Forms.ComboBox();
+            this.OatTemp = new System.Windows.Forms.ComboBox();
+            this.OilPress = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(816, 251);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 23);
+            this.SendButton.TabIndex = 0;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // RightFuel
+            // 
+            this.RightFuel.FormattingEnabled = true;
+            this.RightFuel.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.RightFuel.Location = new System.Drawing.Point(413, 66);
+            this.RightFuel.Name = "RightFuel";
+            this.RightFuel.Size = new System.Drawing.Size(30, 21);
+            this.RightFuel.TabIndex = 2;
+            this.RightFuel.Text = "0";
+            // 
+            // Amps
+            // 
+            this.Amps.FormattingEnabled = true;
+            this.Amps.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.Amps.Location = new System.Drawing.Point(468, 66);
+            this.Amps.Name = "Amps";
+            this.Amps.Size = new System.Drawing.Size(30, 21);
+            this.Amps.TabIndex = 3;
+            this.Amps.Text = "0";
+            // 
+            // LeftFuel
+            // 
+            this.LeftFuel.FormattingEnabled = true;
+            this.LeftFuel.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.LeftFuel.Location = new System.Drawing.Point(356, 66);
+            this.LeftFuel.Name = "LeftFuel";
+            this.LeftFuel.Size = new System.Drawing.Size(30, 21);
+            this.LeftFuel.TabIndex = 4;
+            this.LeftFuel.Text = "0";
+            // 
+            // Tit
+            // 
+            this.Tit.FormattingEnabled = true;
+            this.Tit.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.Tit.Location = new System.Drawing.Point(356, 149);
+            this.Tit.Name = "Tit";
+            this.Tit.Size = new System.Drawing.Size(30, 21);
+            this.Tit.TabIndex = 7;
+            this.Tit.Text = "0";
+            // 
+            // CHT
+            // 
+            this.CHT.FormattingEnabled = true;
+            this.CHT.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.CHT.Location = new System.Drawing.Point(468, 149);
+            this.CHT.Name = "CHT";
+            this.CHT.Size = new System.Drawing.Size(30, 21);
+            this.CHT.TabIndex = 6;
+            this.CHT.Text = "0";
+            // 
+            // OilTemp
+            // 
+            this.OilTemp.FormattingEnabled = true;
+            this.OilTemp.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.OilTemp.Location = new System.Drawing.Point(413, 149);
+            this.OilTemp.Name = "OilTemp";
+            this.OilTemp.Size = new System.Drawing.Size(30, 21);
+            this.OilTemp.TabIndex = 5;
+            this.OilTemp.Text = "0";
+            // 
+            // FuelPress
+            // 
+            this.FuelPress.FormattingEnabled = true;
+            this.FuelPress.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.FuelPress.Location = new System.Drawing.Point(356, 107);
+            this.FuelPress.Name = "FuelPress";
+            this.FuelPress.Size = new System.Drawing.Size(30, 21);
+            this.FuelPress.TabIndex = 10;
+            this.FuelPress.Text = "0";
+            // 
+            // OatTemp
+            // 
+            this.OatTemp.FormattingEnabled = true;
+            this.OatTemp.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.OatTemp.Location = new System.Drawing.Point(468, 107);
+            this.OatTemp.Name = "OatTemp";
+            this.OatTemp.Size = new System.Drawing.Size(30, 21);
+            this.OatTemp.TabIndex = 9;
+            this.OatTemp.Text = "0";
+            // 
+            // OilPress
+            // 
+            this.OilPress.FormattingEnabled = true;
+            this.OilPress.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.OilPress.Location = new System.Drawing.Point(413, 107);
+            this.OilPress.Name = "OilPress";
+            this.OilPress.Size = new System.Drawing.Size(30, 21);
+            this.OilPress.TabIndex = 8;
+            this.OilPress.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(341, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Left fuel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(401, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Right fuel";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(465, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Amps";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(465, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Oat temp";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(401, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Oil press";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(341, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Fuel press";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(465, 133);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "CHT";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(401, 133);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Oil temp";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(341, 133);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(24, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "TIT";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 286);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FuelPress);
+            this.Controls.Add(this.OatTemp);
+            this.Controls.Add(this.OilPress);
+            this.Controls.Add(this.Tit);
+            this.Controls.Add(this.CHT);
+            this.Controls.Add(this.OilTemp);
+            this.Controls.Add(this.LeftFuel);
+            this.Controls.Add(this.Amps);
+            this.Controls.Add(this.RightFuel);
+            this.Controls.Add(this.SendButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.ComboBox RightFuel;
+        private System.Windows.Forms.ComboBox Amps;
+        private System.Windows.Forms.ComboBox LeftFuel;
+        private System.Windows.Forms.ComboBox Tit;
+        private System.Windows.Forms.ComboBox CHT;
+        private System.Windows.Forms.ComboBox OilTemp;
+        private System.Windows.Forms.ComboBox FuelPress;
+        private System.Windows.Forms.ComboBox OatTemp;
+        private System.Windows.Forms.ComboBox OilPress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ SimCockpitGrap/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace SimCockpitGrap
+{
+    public partial class Form1 : Form
+    {
+        char[] led = { '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1' };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region pierdoly
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            FileStream file = new FileStream("C:/Users/Dell/Documents/SimCockpit/Debug/data.txt", FileMode.Create, FileAccess.Write);
+
+            StreamWriter sw = new StreamWriter(file);
+
+            led[0] = (LeftFuel.Text.ToCharArray())[0];
+            led[1] = (RightFuel.Text.ToCharArray())[0];
+            led[2] = (Amps.Text.ToCharArray())[0];
+            led[3] = (FuelPress.Text.ToCharArray())[0];
+            led[4] = (OilPress.Text.ToCharArray())[0];
+            led[5] = (OatTemp.Text.ToCharArray())[0];
+            led[6] = (Tit.Text.ToCharArray())[0];
+            led[7] = (OilTemp.Text.ToCharArray())[0];
+            led[8] = (CHT.Text.ToCharArray())[0];
+
+            sw.Write('o');
+
+            sw.Write(led, 0, 13);
+
+            sw.Close();
+
+            file.Close();
+
+            Directory.SetCurrentDirectory("C:/Users/Dell/Documents/SimCockpit/Debug");
+
+            Process sim = new Process();
+            sim.StartInfo.UseShellExecute = false;
+            sim.StartInfo.FileName = "SimCockpit.exe";
+            //sim.EnableRaisingEvents = true;
+
+            sim.Start();
+
+        }
+    }
+}
